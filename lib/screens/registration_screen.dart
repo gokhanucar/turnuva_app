f6e280520499d70cc141c596d4a7412dfea8a5af
Gokhan Ucar <gokhanucar10@gmail.com>
2021-10-11 22:26:19 +0300

    folders added

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  static const id = 'registration_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
